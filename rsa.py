acea767e65e51f9d148846fadad200a53f0b1e4d
mayankmetha <mayankmethad@gmail.com>
2019-04-25 18:51:43 +0530

    rsa algo

--- rsa.py
+++ rsa.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
 
-# RSA Algo
-# select 2 32=bit prime p and q
-# n = p*q
-# phi(n)
-# relPrime(e,phi(n)) 1 < e < phi(n)
-# d = mulInv(e,phi(n))
-# key1 = (e,n)
-# key2 = (d,n)
-# cipher  = (m**e)mod(n)
-# ptext = (m**d)mod(n)
-
 import sys
 import os
 import random
 import math
-import sympy
 
 #gcd
 def gcd(a,b):
@@ -37,13 +25,9 @@ def extgcd(a,b):
 def mulInv(a,n):
     d,x,y = extgcd(a,n)
     if d == 1:
-        b = 1
-        tmp = x*(b/d)
-        return int(tmp%n)
-    else:
-        return 0
+        return int(x%n)
 
-#eulers totient
+#eulers totient for Prime numbers
 def phi(n):
     result = n
     p = 2
@@ -60,57 +44,18 @@ def phi(n):
 # generates prime numbers
 def genPrime():
     x = 1
-    while not isPrime(x):
-        print(x)
+    while not isPrimeBrute(x):
         x = random.getrandbits(32)
     return x
 
-# AKS primality check 
-# Complexity: Õ((log n)**12)
+# Naive primality check
 # True if prime
-def isPrime(n):
-    # step1: if n = a**b for integers a > 1 and b > 1, output composite.
-    for b in range(2,math.ceil(math.log2(n))):
-        a = n**(1/b)
-        #print(a,b)
-        if a.is_integer():
-            print("Composite")
+def isPrimeBrute(n):
+    if n <= 1:
+        return False
+    for a in range(2,n-1):
+        if n%a == 0:
             return False
-    # step2: Find the smallest r such that multiplicative order of n modulo r > (log2(n))**2.
-    maxk = math.floor((math.log2(n)**2))
-    maxr = max(3,math.ceil((math.log2(n)**5)))
-    nextR = True
-    r = 2
-    while nextR and r < maxr:
-        nextR = False
-        k = 1
-        while not nextR and k <= maxk:
-            t1 = (n**k)%r
-            nextR = (t1 == 1 or t1 == 0)
-            k += 1
-        r += 1
-    r -= 1
-    #print(r)
-    #step3: If 1 < gcd(a,n) < n for some a ≤ r, output composite.
-    a = r
-    while a > 1:
-        g = gcd(a,n)
-        if g > 1 and g < n:
-            return False
-        a -= 1
-        #print(a,n,g)
-    #step4: If n ≤ r, output prime.
-    if n <= r:
-        return True
-    #step5: polynomial modulo congruence based check
-    limit = math.floor(math.sqrt(phi(r))*math.log2(n))
-    for a in range(1,(limit+1)):
-        x = sympy.poly("x")
-        equ = sympy.trunc(sympy.rem((x + a)**n -(x**n + a), x**r - 1), n)
-        #print(a,equ)
-        if equ != 0:
-            return False
-    #step6: output prime otherwise
     return True
 
 # Relative prime check
@@ -120,4 +65,36 @@ def isRelPrime(a,b):
         return True
     else:
         return False
+    
+def main():
+    p = genPrime()
+    q = genPrime()
+    n = p*q
+    phiN = phi(p)*phi(q)
+    e = 0
+    for i in range(2,phiN):
+        e = i
+        if isRelPrime(e,phiN):
+            break
+    d = mulInv(e,phiN)
+    print("Key1=(%d,%d)"%(e,n))
+    print("Key2=(%d,%d)"%(d,n))
+    f = open("rsaKeys","w")
+    f.write(str(e)+","+str(d)+","+str(n))
+    f.close()
+    fin = open("rsaIn","r")
+    fout = open("rsaEnc","w")
+    for _ in fin.readlines():
+        tmp = pow(int(_),e,n)
+        fout.write(str(tmp)+"\n")
+        print("Encrypting %d as %d"%(int(_.strip()),tmp))
+        tmp2 = pow(tmp,d,n)
+        print("Decrypting %d as %d"%(tmp,tmp2))
+        i = 0
+        if int(_.strip()) == tmp2:
+            i += 1
+    print(i)
+    fout.close()
+    fin.close()
 
+main()
